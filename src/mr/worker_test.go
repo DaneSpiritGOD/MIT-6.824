02602ec69b2c69ec3921dda6606cd9fe3fbe8d87
dane <838652739@qq.com>
2021-09-02 13:20:37 +0800

    worker test wip

--- src/mr/worker_test.go
+++ src/mr/worker_test.go
@@ -1,6 +1,7 @@
 package mr
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -11,9 +12,44 @@ func TestSortByGroup(t *testing.T) {
 		}
 
 		d := key[0] - '0'
-		if d < 0 || d > 9 {
-			return 0
+		if d <= 9 {
+			return int(d)
 		}
-		return int(d)
+		return 0
+	}
+
+	data := []KeyValue{
+		{"3", "1"},
+		{"23", "1"},
+		{"1", "1"},
+		{"1", "1"},
+		{"14", "1"},
+		{"16", "1"},
+		{"2354", "1"},
+		{"368", "1"},
+		{"245", "1"},
+		{"1", "1"},
+	}
+
+	expectedResults := [...]keyGroup{
+		{1, KeyValues{"1", []string{"1", "1", "1"}}},
+		{1, KeyValues{"14", []string{"1"}}},
+		{1, KeyValues{"16", []string{"1"}}},
+		{2, KeyValues{"23", []string{"1"}}},
+		{2, KeyValues{"2354", []string{"1"}}},
+		{2, KeyValues{"245", []string{"1"}}},
+		{3, KeyValues{"3", []string{"1"}}},
+		{3, KeyValues{"368", []string{"1"}}},
+	}
+
+	actualResults := sortByGroup(getHashIdFunc, data)
+	if len(expectedResults) != len(actualResults) {
+		t.Errorf("expected len: %d, actual len: %d", len(expectedResults), len(actualResults))
+	}
+
+	for i, e := range expectedResults {
+		if !reflect.DeepEqual(e, *actualResults[i]) {
+			t.Errorf("expected item: %v, actual item: %v not equal", e, *actualResults[i])
+		}
 	}
 }
